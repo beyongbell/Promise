8636ec567413ca2ba89d941a60ea27ea61011327
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2020-06-02 12:25:14 +0700

    Finish ToPromise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,19 +12,48 @@ export class AppComponent {
   constructor(private http: HttpClient) {}
 
   onObservableLoad() {
-
+    this.http.get('http://jsonplaceholder.typicode.com/todos/1').subscribe(result1 => {
+      alert(JSON.stringify(result1));
+      this.http.get('http://jsonplaceholder.typicode.com/todos/2').subscribe(result2 => {
+        alert(JSON.stringify(result2));
+        this.http.get('http://jsonplaceholder.typicode.com/todos/3').subscribe(result3 => {
+          alert(JSON.stringify(result3));
+        });
+      });
+    });
   }
 
   onPromiseLoad() {
-
+    this.http.get('http://jsonplaceholder.typicode.com/todos/1').toPromise().then(result => {
+      alert(JSON.stringify(result));
+    }).then(() => {
+      alert('2');
+    }).then(() => {
+      alert('3');
+    }).then(() => {
+      alert('4');
+    });
   }
 
   onPromiseMultipleLoad() {
+    const p1 = this.http.get('http://jsonplaceholder.typicode.com/todos/1').toPromise();
+    const p2 = this.http.get('http://jsonplaceholder.typicode.com/todos/2').toPromise();
+    const p3 = this.http.get('http://jsonplaceholder.typicode.com/todos/3').toPromise();
 
+    Promise.all([p1, p2, p3]).then(results => {
+      alert(JSON.stringify(results[0]));
+      alert(JSON.stringify(results[1]));
+      alert(JSON.stringify(results[2]));
+    });
   }
 
-  onAsyncAwaitLoad() {
-
+  async onAsyncAwaitLoad() {
+    const result1 = await this.http.get('http://jsonplaceholder.typicode.com/todos/1').toPromise();
+    alert(JSON.stringify(result1));
+    const result2 = await this.http.get('http://jsonplaceholder.typicode.com/todos/2').toPromise();
+    alert(JSON.stringify(result2));
+    const result3 = await this.http.get('http://jsonplaceholder.typicode.com/todos/3').toPromise();
+    alert(JSON.stringify(result3));
   }
 
 }
